d6693272506f53cf0a23fd4463514cb99ef94605
Diana Gruzdeva <taciturn.homebody@gmail.com>
2021-03-29 12:49:05 +0300

    add try...catch blocks, refactor code

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 (function() {
   const defaultLog = console.log;
+
   console.log = function() {
     const message = [];
     while (arguments.length) {
       message.push("We got the following from Stack: " +
       [].shift.call(arguments));
     }
+
     defaultLog.apply(console, message);
   };
 
   const defaultWarn = console.warn;
+
   console.warn = function() {
     const message = [];
     while (arguments.length) {
       message.push("Warning from Stack! " + [].shift.call(arguments));
     }
+
     defaultWarn.apply(console, message);
   };
 })();
@@ -35,98 +39,116 @@ class Stack {
   }
 
   isEmpty() {
-    return this.top == -1;
+    if (this.top == -1) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   pop() {
     if (this.isEmpty() == false) {
       this.top--;
       return this.data.pop();
+    } else {
+      throw "Stack is empty!";
     }
   }
 
   peek() {
-    return this.data[this.top];
-  }
-}
-
-let stack = null;
-
-function reverse_array(array) {
-  const reversedArray = [];
-  for (let i = 0; i < array.length; i++) {
-    reversedArray[i] = array[(array.length - 1) - i];
-  }
-  return reversedArray;
-}
-
-function get_data() {
-  const data = this.data;
-  console.log("Data from stack is " + reverse_array(data));
-  return reverse_array(data);
-}
-
-function get_top() {
-  const top = this.top;
-  console.log("Top element is " + top);
-  return top;
-}
-
-function create_stack() {
-  if (stack == null) {
-    stack = new Stack();
-    document.getElementById("createStack").disabled = true;
-    document.getElementById("fillStack").disabled = false;
-    document.getElementById("elementField").disabled = false;
-    document.getElementById("addElement").disabled = false;
-    document.getElementById("peekElement").disabled = false;
-    document.getElementById("popElement").disabled = false;
-    document.getElementById("printStack").disabled = false;
-    document.getElementById("stackField").disabled = false;
-    document.getElementById("clearStack").disabled = false;
-  }
-}
-
-function fill_stack() {
-  if (stack != null) {
-    for (let i = 0; i < 10; i++) {
-      stack.push(Math.floor(Math.random() * 11));
+    if (this.isEmpty() == false) {
+      return this.data[this.top];
+    } else {
+      throw "Stack is empty!";
     }
   }
 }
 
-function add_element() {
-  if (stack != null) {
-    let value = document.getElementById("elementField").value;
-    stack.push(value);
-    document.getElementById("elementField").value = "";
+window.onload=function() {
+  document.getElementById("createStack").addEventListener("click", createStack);
+  document.getElementById("fillStack").addEventListener("click", fillStack);
+  document.getElementById("addElement").addEventListener("click", addElement);
+  document.getElementById("peekElement").addEventListener("click", peekElement);
+  document.getElementById("popElement").addEventListener("click", popElement);
+  document.getElementById("printStack").addEventListener("click", printStack);
+  document.getElementById("clearStack").addEventListener("click", clearStack);
+
+  const elements = document.querySelectorAll(".controls");
+
+  for (const elem of elements) {
+    elem.disabled = true;
+  }
+  document.getElementById("createStack").disabled = false;
+};
+
+let stack = null;
+
+function reverseArray(array) {
+  const reversedArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    reversedArray[i] = array[(array.length - 1) - i];
+  }
+
+  return reversedArray;
+}
+
+function getData() {
+  const data = this.data;
+  console.log("Data from stack is " + reverseArray(data));
+  return reverseArray(data);
+}
+
+function createStack() {
+  stack = new Stack();
+  const elements = document.querySelectorAll(".controls");
+
+  for (const elem of elements) {
+    elem.disabled = false;
+  }
+  document.getElementById("createStack").disabled = true;
+}
+
+function fillStack() {
+  for (let i = 0; i < 10; i++) {
+    stack.push(Math.floor(Math.random() * 11));
   }
 }
 
-function peek_element() {
-  if (stack != null && stack.isEmpty() == false) {
-    const result = get_top.call(stack);
-  } else if (stack.isEmpty != false) {
-    console.warn("Stack is empty!");
+function addElement() {
+  const value = document.getElementById("elementField").value;
+  stack.push(value);
+  document.getElementById("elementField").value = "";
+}
+
+function peekElement() {
+  try {
+    const result = stack.peek();
+    console.log("Top element is " + result);
+  } catch (e) {
+    console.warn(e);
   }
 }
 
-function pop_element() {
-  if (stack != null && stack.isEmpty() == false) {
+function popElement() {
+  try {
     console.log("Top element was " + stack.pop());
-  } else if (stack.isEmpty != false) {
-    console.warn("Stack is empty!");
+  } catch (e) {
+    console.warn(e);
   }
 }
 
-function print_stack() {
-  if (stack != null && stack.isEmpty() == false) {
-    let result = get_data.apply(stack);
+function printStack() {
+  if (stack.isEmpty() == false) {
+    let result = getData.apply(stack);
     result += "";
-    let finalResult = result.split(",");
+    const splittedResult = result.split(",");
     result = "";
     for (let i = 0; i < stack.length(); i++) {
-      result += finalResult[i] + "\n";
+      result += splittedResult[i] + "\n";
+    }
+    if (result == "") {
+      console.warn("Stack is empty!");
     }
     document.getElementById("stackField").value = result;
   } else if (stack.isEmpty != false) {
@@ -134,14 +156,14 @@ function print_stack() {
   }
 }
 
-function clear_stack() {
-  if (stack != null && stack.isEmpty() == false) {
+function clearStack() {
+  if (stack.isEmpty() == false) {
     while (stack.isEmpty() == false) {
       stack.pop();
     }
     document.getElementById("stackField").value = "";
     console.log("Stack is cleared");
-  } else if (stack.isEmpty != false) {
+  } else {
     console.warn("Stack is empty!");
   }
 }
